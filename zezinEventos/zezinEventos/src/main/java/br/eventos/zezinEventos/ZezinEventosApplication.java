2ae972bb0d5af09e404f2bc265eb3b6706bdcd06
pedro carvalho <pedro@example.com>
2025-06-05 16:49:09 -0300

    criando projeto

---
+++ zezinEventos/zezinEventos/src/main/java/br/eventos/zezinEventos/ZezinEventosApplication.java
@@ -0,0 +1,13 @@
+package br.eventos.zezinEventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZezinEventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZezinEventosApplication.class, args);
+	}
+
+}
---
+++ zezinEventos/zezinEventos/src/test/java/br/eventos/zezinEventos/ZezinEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.eventos.zezinEventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZezinEventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
